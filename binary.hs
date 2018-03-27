817380e1ad30945dd79f43243186430ed121fdc4
EGoms <evangomolin@gmail.com>
2018-03-27 11:30:07 -0400

    chained function calls

--- binary.hs
+++ binary.hs
@@ -6,7 +6,7 @@ main = do
         else if x == 8
             then do octal
             else if x == 16
-                then do putStrLn "Hex not yet supported"
+                then do hex
                 else do return ()
     
     
@@ -23,9 +23,9 @@ binary = do
     let binaryList = [2^n | n <- [0..10]]
     line <- promptLine "Enter binary number"
     let number = (read line :: Int)
-    let c = reverse (digits number)
-    let d = zipWith (*) c binaryList
-    print (sum(d))
+    --let c = reverse (digits number)
+    let d = sum (zipWith (*) (reverse . digits $ number) binaryList) --digits applied to number, reverse to that, zipwith with binary list then sum it
+    print (d)
     main
     
 octal :: IO ()
@@ -33,7 +33,16 @@ octal = do
     let octalList = [8^n | n <- [0..10]]
     line <- promptLine "Enter octal number"
     let number = (read line :: Int)
-    let c =  reverse (digits number)
-    let d = zipWith (*) c octalList
-    print (sum(d))
+    --let c =  reverse (digits number)
+    print (sum (zipWith (*) (reverse . digits $ number) octalList)) 
+    --print (d)
+    main
+    
+hex :: IO ()
+hex = do
+    let hexList = [16^n | n<- [0..10]]
+    line <- promptLine "Enter hexadecimal number"
+    let number = (read line :: Int)
+    let d = sum (zipWith (*) (reverse . digits $ number) hexList)
+    print(d)
     main
