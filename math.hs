33ef888f4aa77b688ea533caaa3b6e7eb4ba0980
Evan G <evangomolin@gmail.com>
2019-07-20 10:14:44 -0400

    Update math.hs

--- math.hs
+++ math.hs
@@ -1,6 +1,7 @@
 import Data.List
 import Data.Bool
 import Data.Complex (Complex((:+)), magnitude)
+import Data.Array
 
 pythag :: (Floating a) => a -> a -> a
 pythag x y = sqrt (x**2 + y**2)
@@ -68,3 +69,14 @@ run =
         [ [ bool ' ' '*' (2 > magnitude (mandelbrot (x :+ y)))
           | x <- [-2,-1.9685 .. 0.5] ]
         | y <- [1, 0.95 .. -1] ]
+
+lcs xs ys = a!(0,0) where
+  n = length xs
+  m = length ys
+  a = array ((0,0),(n,m)) $ l1 ++ l2 ++ l3
+  l1 = [((i,m),[]) | i <- [0..n]]
+  l2 = [((n,j),[]) | j <- [0..m]]
+  l3 = [((i,j), f x y i j) | (x,i) <- zip xs [0..], (y,j) <- zip ys [0..]]
+  f x y i j 
+    | x == y    = x : a!(i+1,j+1)
+    | otherwise = longest (a!(i,j+1)) (a!(i+1,j))
