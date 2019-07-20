857ffdcc97f7f165639fc9b30c117da6b4d15cc3
Evan G <evangomolin@gmail.com>
2019-07-20 10:07:13 -0400

    Update math.hs

--- math.hs
+++ math.hs
@@ -1,4 +1,6 @@
 import Data.List
+import Data.Bool
+import Data.Complex (Complex((:+)), magnitude)
 
 pythag :: (Floating a) => a -> a -> a
 pythag x y = sqrt (x**2 + y**2)
@@ -49,3 +51,15 @@ egcd a b = (t, s-q*t)
         (s, t) = egcd b r
         
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)        
+
+
+mandelbrot :: RealFloat a => Complex a -> Complex a
+mandelbrot a = iterate ((a+) . (^ 2)) 0 !! 50
+
+run :: IO ()
+run = 
+    mapM_
+        putStrLn
+        [ [ bool ' ' '*' (2 > magnitude (mandelbrot (x :+ y)))
+          | x <- [-2,-1.9685 .. 0.5] ]
+        | y <- [1, 0.95 .. -1] ]
